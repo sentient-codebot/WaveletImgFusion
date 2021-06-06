e5a0decb2d744924df97c5423f9f72abfa9a88eb
Nan Lin <n.lin-2@student.tudelft.nl>
2021-06-06 23:57:06 +0200

    fast guided filtering done

--- fast_guided_filter.m
+++ fast_guided_filter.m
@@ -4,17 +4,34 @@ function O = fast_guided_filter(P, I, kwargs)
         I % must be of same size
         kwargs.r (1,1) {mustBeInteger,mustBeNonnegative} = 1
         kwargs.eps (1,1) {mustBeNonnegative} = 0.001
+        kwargs.s (1,1) {mustBeInteger,mustBePositive} = 2
+        kwargs.SubMethod (1,1) string = 'nearest'
+        kwargs.UpMethod (1,1) string = 'cubic'
     end
     r = kwargs.r;
     eps = kwargs.eps;
-    win_size = (2*r+1); % window: size x size
-    input_size = size(P);
+    s = kwargs.s;
+    SubMethod = kwargs.SubMethod;
+    UpMethod = kwargs.UpMethod;
+    
+    img_size = size(I);
+    if s>1
+        [X,Y] = meshgrid(1:img_size(2),1:img_size(1));
+        [X_sub,Y_sub] = meshgrid(1:s:img_size(2),1:s:img_size(1));
+        I_sub = interp2(X,Y,I,X_sub,Y_sub,SubMethod);
+        P_sub = interp2(X,Y,P,X_sub,Y_sub,SubMethod);
+        r_sub = floor(r/s);
+    else
+        I_sub = I;
+        P_sub = P;
+        r_sub = r;
+    end
     
     % coefficients
-    mean_I = img_avg(I,r);
-    mean_P = img_avg(P,r);
-    corr_I = img_avg(I.*I,r);
-    corr_IP = img_avg(I.*P,r);
+    mean_I = img_avg(I_sub,r_sub);
+    mean_P = img_avg(P_sub,r_sub);
+    corr_I = img_avg(I_sub.*I_sub,r_sub);
+    corr_IP = img_avg(I_sub.*P_sub,r_sub);
     
     var_I = corr_I - mean_I.*mean_I;
     cov_IP = corr_IP - mean_I.*mean_P;
@@ -24,7 +41,10 @@ function O = fast_guided_filter(P, I, kwargs)
     
     A_bar = img_avg(A,r);
     B_bar = img_avg(B,r);
-    
+    if s>1
+        A_bar = interp2(X_sub,Y_sub,A_bar,X,Y,UpMethod);
+        B_bar = interp2(X_sub,Y_sub,B_bar,X,Y,UpMethod);
+    end
     % output
     O = A_bar.*I + B_bar;
     
