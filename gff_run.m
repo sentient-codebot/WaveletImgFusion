bd358a9b7b44250c633e2181dfbec335fe3c9935
Nan Lin <n.lin-2@student.tudelft.nl>
2021-06-11 19:19:20 +0200

    organize

--- gff_run.m
+++ gff_run.m
@@ -1,9 +1,9 @@
 
 %% load image
 % 
-file_name = "images/source22_";
-fig_ori1 = imread("source22_1.tif");
-fig_ori2 = imread("source22_2.tif");
+file_name = "images/clock_";
+fig_ori1 = imread(file_name+"1.tif");
+fig_ori2 = imread(file_name+"2.tif");
 
 % file_name = "images/desk_";
 % fig_origin1 = imread(file_name+"far.tif");
@@ -38,4 +38,4 @@ subplot(1,3,3)
 imshow(fig)
 
 %% write fused image to file
-imwrite(fig,file_name+'fused.png')
+imwrite(fig,file_name+'gff.png')
